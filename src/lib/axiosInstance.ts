109a419be625c7174695389fcf7f2ff1507521f6
ARslan_Ahm <mycomputer2214@gmail.com>
2025-03-19 11:10:24 +0500

    [fix] Changed the middleware and ready to test it out.

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getSession } from "next-auth/react";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://medicare-dashboard-arslan.vercel.app/api/",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api/",
 });
 
 api.interceptors.request.use(
